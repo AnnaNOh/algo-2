740c9f9a2d62a10bff4be0913435d8f43e37b1b6
Anna <annaoh1994@gmail.com>
2018-01-02 00:26:41 -0800

    p02 finished except Hash

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    sum = 0
+    self.each_with_index do |el, i|
+      sum += (el.hash * i)
+    end
+    return sum
   end
 end
 
 class String
   def hash
+    sum = 0
+    self.chars.each_with_index do |el, i|
+      sum += (el.ord.hash * i)
+    end
+    return sum
   end
 end
 
@@ -16,6 +26,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.hash
   end
 end
